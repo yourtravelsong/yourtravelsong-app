5b09ccd8a8e76bb70ed54873f7b4600719854416
Matias Martinez <matomartinez@gmail.com>
2024-06-29 18:24:23 +0200

    integration backend with models and external APIs

--- UnitTests.py
+++ UnitTests.py
@@ -1,7 +1,9 @@
+import time
 import unittest
 import http.client
 import json
 import requests
+import ollama
 class UnitTestCase(unittest.TestCase):
 
     def test_SingleCall(self):
@@ -16,6 +18,126 @@ class UnitTestCase(unittest.TestCase):
         self.assertIsNotNone("success", res['status'])
         print("Result",res['result'])
         self.assertIsNotNone(res['result'])
+        self.assertIsNotNone(res['result']['offers'])
+        self.assertIsNotNone(res['result']['sentiments'])
+
+
+    def test_ollama_chat(self):
+        response = ollama.chat(model='llama3', messages=[
+            {
+                'role': 'user',
+                'content': 'continue the code, put just code, no explanation: def helloWord():\n  ',
+            },
+        ])
+        print(response)
+
+        print(response['message']['content'])
+        self.assertTrue(len(response['message']['content']) > 0)
+
+    def test_ollama_complete(self):
+        response = ollama.generate(model='llama3',
+                                   prompt='continue the code, put just code, no explanation: def helloWord():\n  ')
+        print(response)
+
+        print(response['response'])
+        self.assertTrue(len(response['response']) > 0)
+
+    def test_ollama_sentiment_mistral(self):
+        init = time.time()
+        lyric = "I'm a creep, I'm a weirdo. What the hell am I doing here? I don't belong here."
+
+        response = ollama.generate(model='mistral',
+                                   prompt='return the sentiments you find in this lyric, just the sentiments, separated by commas:\n  {} '.format(
+                                       lyric))
+        print(response)
+
+        print(response['response'])
+        self.assertTrue(len(response['response']) > 0)
+        end = time.time()
+        print("Time to execute: ", (end - init) )
+
+        init = time.time()
+        lyric = "I'm a creep, I'm a weirdo. What the hell am I doing here? I don't belong here."
+
+        response = ollama.generate(model='mistral',
+                                   prompt='return the sentiments you find in this lyric, just the sentiments, separated by commas:\n  {} '.format(
+                                       lyric))
+        print(response)
+
+        print(response['response'])
+        self.assertTrue(len(response['response']) > 0)
+        end = time.time()
+        print("Time to execute expressed in seconds : ",(end - init) )
+
+    def test_ollama_sentiment(self):
+
+        lyric = "I'm a creep, I'm a weirdo. What the hell am I doing here? I don't belong here."
+
+        response = ollama.generate(model='llama3',
+                                   prompt='return the sentiments you find in this lyric, just the sentiments, separated by commas:\n  {} '.format(lyric))
+        print(response)
+
+        print(response['response'])
+        self.assertTrue(len(response['response']) > 0)
+
+        lyric2='''Driving faster in my car
+Falling farther from just what we are
+Smoke a cigarette and lie some more
+These conversations kill
+Falling faster in my car
+
+Time to take her home,
+Her dizzy head is conscience laden
+Time to take a ride it leaves today
+No conversation
+Time to take her home her dizzy head is
+Conscience laden
+Time to wait too long, to wait too long
+To wait too long
+
+Too much walking shoes worn thin
+Too much trippin' and my soul's worn thin
+Time to catch her ride it leaves today
+Her name is what it means
+Too much walking shoe worn thin
+Time to take her home
+Her dizzy head is conscience laden
+Time to take a ride it leaves today
+No conversation
+
+Time to take her home her dizzy head is
+Conscience laden
+Time to wait too long, to wait too long
+To wait too long
+
+Conversations kill
+Conversations kill
+Conversations kill
+
+Time to take her home,
+Her dizzy head is conscience laden
+Time to take a ride it leaves today
+No conversation
+Time to take her home her dizzy head is
+Conscience laden
+Time to wait too long, to wait too long
+To wait too long
+
+Conversations kill
+Conversations kill
+Conversations kill'''
+
+        response = ollama.generate(model='llama3',
+                                   prompt='return the sentiments you find in this lyric, just the sentiments, separated by commas:\n  {} '.format(
+                                       lyric2))
+        print(response)
+
+        print(response['response'])
+        self.assertTrue(len(response['response']) > 0)
+
+    def test_ListModels(self):
+        for l in ollama.list()["models"]:
+            print(l)
 
 
 if __name__ == '__main__':
