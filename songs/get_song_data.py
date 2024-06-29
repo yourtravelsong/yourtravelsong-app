4061c8328bc8d561f0ced690d04ce41997e21660
Manel <121304227+manelpalacdi@users.noreply.github.com>
2024-06-29 15:49:28 +0200

    Add files via upload

--- songs/get_song_data.py
+++ songs/get_song_data.py
@@ -1,12 +1,14 @@
 from datasets import load_dataset
 import os
 import pandas as pd
+import json
 
-ds = load_dataset("zsoltardai/spotify-tracks")
+ds = load_dataset("zsoltardai/spotify-tracks", split="train")
 os.makedirs('../data/songs', exist_ok=True)
-for split in ds.keys():
+ds.to_csv("../data/songs/song_train.csv")
+""" for split in ds.keys():
     df = ds[split].to_pandas()
-    df.to_csv(f'../data/songs/song_{split}.csv', index=False)
+    df.to_json(f'../data/songs/song_{split}.json', index=False)
 
 df = df.sample(frac=1).reset_index(drop=True)
 df = df.head(1000)
@@ -14,4 +16,4 @@ df = df.head(1000)
 for i in df.index:
     file = open(f"../data/songs/{df.loc[i, "artist"]}-{df.loc[i, "song"]}", "w")
     file.write(f"{df.loc[i, "cleaned_text"]}")
-    file.close()
+    file.close() """
