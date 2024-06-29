ae4b2ae5f1b094bfd6b0e2d9dff1e847b6dc951c
Manel <121304227+manelpalacdi@users.noreply.github.com>
2024-06-29 15:28:37 +0200

    Add files via upload

--- songs/get_song_data.py
+++ songs/get_song_data.py
@@ -7,3 +7,11 @@ os.makedirs('../data/songs', exist_ok=True)
 for split in ds.keys():
     df = ds[split].to_pandas()
     df.to_csv(f'../data/songs/song_{split}.csv', index=False)
+
+df = df.sample(frac=1).reset_index(drop=True)
+df = df.head(1000)
+
+for i in df.index:
+    file = open(f"../data/songs/{df.loc[i, "artist"]}-{df.loc[i, "song"]}", "w")
+    file.write(f"{df.loc[i, "cleaned_text"]}")
+    file.close()
