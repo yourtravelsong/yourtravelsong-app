f85bf5f6851d49d39b59e83e8ab8ed02df06f1f2
Matias Martinez <matomartinez@gmail.com>
2024-06-29 19:29:52 +0200

    back refactoring

--- Backend.py
+++ Backend.py
@@ -3,92 +3,93 @@ import os
 import json
 from amadeus import Client, ResponseError
 
-def retrieveLyric(artist, song):
-    ## todo
-    print("WARNING, HARDCODED LYRIC")
-    return "I'm a creep, I'm a weirdo. What the hell am I doing here? I don't belong here."
+class TravelBackend:
 
-def retrieveCity(artist, song):
-    ## todo
-    print("WARNING, HARDCODED CITY")
-    return "New York"
+    def __init__(self):
+        print("Init TravelBackend")
 
-amadeus = Client(
-    client_id=os.environ['AMADEUS_API_KEY'],
-    client_secret=os.environ['AMADEUS_SECRET_KEY']
-)
 
-def computeIATACode(model, obtainedCity):
-    obtain_sentiments_list = model.complete("which is the IATA code of {} airport? return just the code".format(obtainedCity))
-    print("Sentiments retrieved: ", obtain_sentiments_list.text)
+    def retrieveLyric(self, artist, song):
+        ## todo
+        print("WARNING, HARDCODED LYRIC")
+        return "I'm a creep, I'm a weirdo. What the hell am I doing here? I don't belong here."
 
-    return obtain_sentiments_list.text.strip()
+    def retrieveCity(self, artist, song):
+        ## todo
+        print("WARNING, HARDCODED CITY")
+        return "New York"
 
-def getAirlineCode(airlineCode):
-    try:
-        response = amadeus.get('/v1/reference-data/airlines', airlineCodes=airlineCode)
-        print("Airlines response {}".format( response))
-        if len(response.data) > 0:
-            return response.data[0]["businessName"]
-        else:
-            return "Unknown-Airline-Code"
-    except ResponseError as error:
-        print(error)
-        return None
-def get_suggestion(artist, song):
-
-    retrievedLyric = retrieveLyric(artist, song)
-
-    llama = Ollama(
-        model="llama3",
-       # request_timeout=40.0,
+    amadeus = Client(
+        client_id=os.environ['AMADEUS_API_KEY'],
+        client_secret=os.environ['AMADEUS_SECRET_KEY']
     )
 
-    obtain_sentiments_list = llama.complete(f"return the sentiments you find in this lyric, just the sentiments, no more words, separated by commas:\n  {retrievedLyric}")
-    print("Sentiments retrieved: ",obtain_sentiments_list.text)
-    sentimentsFromLyric = obtain_sentiments_list.text.split(",")
+    def computeIATACode(self,model, obtainedCity):
+        obtain_sentiments_list = model.complete("which is the IATA code of {} airport? return just the code".format(obtainedCity))
+        print("Sentiments retrieved: ", obtain_sentiments_list.text)
+
+        return obtain_sentiments_list.text.strip()
+
+    def getAirlineCode(self,airlineCode):
+        try:
+            response = self.amadeus.get('/v1/reference-data/airlines', airlineCodes=airlineCode)
+            print("Airlines response {}".format( response))
+            if len(response.data) > 0:
+                return response.data[0]["businessName"]
+            else:
+                return "Unknown-Airline-Code"
+        except ResponseError as error:
+            print(error)
+            return None
+    def get_suggestion(self, artist, song):
+
+        retrievedLyric = self.retrieveLyric(artist, song)
+
+        llama = Ollama(
+            model="llama3",
+           # request_timeout=40.0,
+        )
+
+        obtain_sentiments_list = llama.complete(f"return the sentiments you find in this lyric, just the sentiments, no more words, separated by commas:\n  {retrievedLyric}")
+        print("Sentiments retrieved: ",obtain_sentiments_list.text)
+        sentimentsFromLyric = obtain_sentiments_list.text.split(",")
 
 
-    obtainedCity = retrieveCity(artist, song)
+        obtainedCity = self.retrieveCity(artist, song)
 
-    IATAcode = computeIATACode(llama, obtainedCity)
-    print("IATA code: ", IATAcode, " for city: ", obtainedCity)
+        IATAcode = self.computeIATACode(llama, obtainedCity)
+        print("IATA code: ", IATAcode, " for city: ", obtainedCity)
 
-    print("sentimentsFromLyric", sentimentsFromLyric)
+        print("sentimentsFromLyric", sentimentsFromLyric)
 
-    dataflights = getFlights(IATAcode, "MAD", departureDate='2024-11-01', adults=1)
+        dataflights = self.getFlights(IATAcode, "MAD", departureDate='2024-11-01', adults=1)
 
-    return {"artist": artist, "song": song, "city_sugg": "X", "sentiments": sentimentsFromLyric, "offers": dataflights}
+        return {"artist": artist, "song": song, "city_sugg": "X", "sentiments": sentimentsFromLyric, "offers": dataflights}
 
 
-def getFlights(codeDestination, codeOrigin, departureDate='2024-11-01', adults=1, topFlights=3):
+    def getFlights(self, codeDestination, codeOrigin, departureDate='2024-11-01', adults=1, topFlights=3):
 
-    try:
-        response = amadeus.shopping.flight_offers_search.get(
-            originLocationCode=codeOrigin,
-            destinationLocationCode=codeDestination,
-            departureDate=departureDate,
-            adults=adults)
-        # print(response.data)
+        try:
+            response = self.amadeus.shopping.flight_offers_search.get(
+                originLocationCode=codeOrigin,
+                destinationLocationCode=codeDestination,
+                departureDate=departureDate,
+                adults=adults)
+            if len(response.data) > 0:
+                resultAllFlights = []
 
-        dataString = json.dumps(response.data, indent=4)
-        print(dataString)
+                print("Flight found")
 
-        if len(response.data) > 0:
-            resultAllFlights = []
-
-            print("Flight found")
-
-            for i in range(min(topFlights, len(response.data)) ):
-                firstFlight = response.data[i]
-                price = firstFlight['price']['total']
-                currency = firstFlight['price']['currency']
-                airlinecode = firstFlight['validatingAirlineCodes']
-                airlinename = getAirlineCode(airlinecode)
-                print("Price: ", price, currency, " Airline_code: ", airlinecode, " Airline_name: ",airlinename)
-                aFlight = { "type": "flight", "airline_name":airlinename, "price": price,   "currency": currency, "departure": departureDate,"airline_code": airlinecode,"status": "success","i":i}
-                resultAllFlights.append(aFlight)
-            return resultAllFlights
-    except ResponseError as error:
-        print("Error retrieving flights {}".format(error))
-        return  None
+                for i in range(min(topFlights, len(response.data)) ):
+                    firstFlight = response.data[i]
+                    price = firstFlight['price']['total']
+                    currency = firstFlight['price']['currency']
+                    airlinecode = firstFlight['validatingAirlineCodes']
+                    airlinename = self.getAirlineCode(airlinecode)
+                    print("Price: ", price, currency, " Airline_code: ", airlinecode, " Airline_name: ",airlinename)
+                    aFlight = { "type": "flight", "airline_name":airlinename, "price": price,   "currency": currency, "departure": departureDate,"airline_code": airlinecode,"status": "success","i":i}
+                    resultAllFlights.append(aFlight)
+                return resultAllFlights
+        except ResponseError as error:
+            print("Error retrieving flights {}".format(error))
+            return  None
