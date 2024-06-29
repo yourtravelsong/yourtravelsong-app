26666305711339c6bc484efb274d5c719963ef76
Manel Palacin <manelpalacdi@gmail.com>
2024-06-29 20:02:18 +0200

    added travel functionality

--- app_demo.py
+++ app_demo.py
@@ -1,19 +1,28 @@
+# run with: streamlit run app_demo.py
 import streamlit as st
 import requests
 import os
 
-def print_city_img(city: str):
-    api_key = os.getenv("UNSPLASH_API_KEY") 
-    search_url = f"https://api.unsplash.com/search/photos?query={city}&client_id={api_key}"
-    
-    response = requests.get(search_url)
-    results = response.json().get("results")
-    
-    if results:
-        image_url = results[0]["urls"]["regular"]
-        image_response = requests.get(image_url)
+API_KEY = os.getenv("UNSPLASH_API_KEY")
+
+def travel_plan():
+    st.divider()
+    st.markdown("Would you like to know your :gray-background[personalized travel plan?] :airplane_departure: ")
+    if (st.button("Show me!")):
+        st.write("example text")
+
+def print_city_img(city: str, show: bool) -> None:
+    if (show):
+        search_url = f"https://api.unsplash.com/search/photos?query={city}&client_id={API_KEY}"
         
-        st.image(image_response.content)
+        response = requests.get(search_url)
+        results = response.json().get("results")
+        
+        if results:
+            image_url = results[0]["urls"]["regular"]
+            image_response = requests.get(image_url)
+            
+            st.image(image_response.content)
 
 def write_results(cities: list[str], expl: str) -> None:
     st.divider()
@@ -21,13 +30,15 @@ def write_results(cities: list[str], expl: str) -> None:
     st.write(f"## {cities[0]}")
     st.write("")
     st.caption(expl)
-    st.write("Other recommendations:")
+    print_city_img(cities[0], show=True) # false to not waste credits for the unsplash api
+    st.write("#### Other recommendations:")
     for city in cities[1:]:
-        st.write(f"{city}")
+        st.write(f"· {city}")
+
 
 def start_exec(song: str, author: str):
     recommended_cities = ["Denver", "Cincinatti", "Detroit"]
-    result_explanation: str = "This would be a great location for your travel! It includes some museums in pop art and other activities you might fancy."
+    result_explanation: str = "This would be a *great* location for your travel! It includes some museums in pop art and other activities you might fancy."
     req_completed = False
     #request output from our program and set req_completed to true (NOW HARDCODED)
     req_completed = True
@@ -51,11 +62,9 @@ def main() -> None:
     st.title("YourTravelSong")
     st.divider()
     song, author = get_input()
-    recommended_cities, result_explanation = start_exec(song, author)
-    write_results(recommended_cities, result_explanation)
-    print_city_img(recommended_cities[0])
-    
-
+    cities, explanation = start_exec(song, author)
+    write_results(cities, explanation)
+    travel_plan()
 
 if __name__ == "__main__":
     main()
