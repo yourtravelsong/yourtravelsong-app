2f9ef5e40d5b4677ca1e7d5a1ba87f238ccf5d91
EyuelPersonal <eyuelwolde@gmail.com>
2024-06-30 10:43:45 +0200

    refinmetns

--- embeddings/custom_query_engine.py
+++ embeddings/custom_query_engine.py
@@ -1,6 +1,8 @@
 from llama_index.core.response_synthesizers import BaseSynthesizer
 from llama_index.core.query_engine import CustomQueryEngine
 from llama_index.core.retrievers import BaseRetriever
+from llama_index.core import PromptTemplate
+from llama_index.llms.mistralai import MistralAI
 
 class RAGQueryEngine(CustomQueryEngine):
     """RAG Query Engine."""
@@ -12,3 +14,23 @@ class RAGQueryEngine(CustomQueryEngine):
         nodes = self.retriever.retrieve(query_str)
         response_obj = self.response_synthesizer.synthesize(query_str, nodes)
         return response_obj
+    
+
+
+class RAGStringQueryEngine(CustomQueryEngine):
+    """RAG String Query Engine."""
+
+    retriever: BaseRetriever
+    response_synthesizer: BaseSynthesizer
+    llm: MistralAI
+    qa_prompt: PromptTemplate
+
+    def custom_query(self, query_str: str):
+        nodes = self.retriever.retrieve(query_str)
+
+        context_str = "\n\n".join([n.node.get_content() for n in nodes])
+        response = self.llm.complete(
+            self.qa_prompt.format(context_str=context_str, query_str=query_str)
+        )
+
+        return str(response)
