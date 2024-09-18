ccaded84abd7ca8aa1afe13ba708463585a9ba2b
Matias Martinez <matomartinez@gmail.com>
2024-09-18 15:54:52 +0200

    Update Unit test

--- UnitTests.py
+++ UnitTests.py
@@ -1,27 +1,32 @@
 import time
 import unittest
-import http.client
 import json
 import requests
 import ollama
+
 class UnitTestCase(unittest.TestCase):
 
-    def test_SingleCall(self):
+    def test_Suggestion_SingleCall(self):
         response = requests.post(headers={"Content-Type": "application/json"},
                                  json={'artist': 'Radiohead', 'title': 'Creep'},
-                                 # json=serialized,
                                  url="http://127.0.0.1:5000/getsuggestion")
-        #print(response.content)
 
         res = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone("success", res['status'])
-        #print("Result",res['result'])
         self.assertIsNotNone(res['result'])
-        self.assertIsNotNone(res['result']['offers'])
+        self.assertIsNotNone(res['result']['recommendations'])
         self.assertIsNotNone(res['result']['sentiments'])
+        self.assertTrue(len(res['result']['recommendations']) > 0)
+        self.assertTrue(len(res['result']['sentiments']) > 0)
+
+        self.assertTrue("offers" in res['result']['recommendations'][0])
+        self.assertTrue(len(res['result']['recommendations'][0]["offers"]) > 0 )
+
         dataString = json.dumps(res, indent=4)
         print(dataString)
+
+
     def test_ollama_chat(self):
         response = ollama.chat(model='llama3', messages=[
             {
