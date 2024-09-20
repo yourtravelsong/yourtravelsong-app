00496db96fd4601390a99e277e96f5bcc6cfbfc1
Matias Martinez <matomartinez@gmail.com>
2024-09-20 13:38:59 +0200

    refactor test for MongoDB

---
+++ test/TestDB.py
@@ -0,0 +1,84 @@
+import logging
+import os
+import unittest
+import certifi
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+from pymongo.cursor import Cursor
+from pymongo.mongo_client import MongoClient
+
+logger = logging.getLogger(__name__)
+
+class Arguments(BaseSettings):
+      env_file:str = "../index.env"
+      log_level: str = "DEBUG"
+
+class TestDatabases(unittest.TestCase):
+
+    def setUp(self):
+        self.argument = Arguments()
+        logging.basicConfig(level=logging.getLevelName(self.argument.log_level))
+        print("Env file ", self.argument.env_file)
+
+        load_dotenv(self.argument.env_file)
+
+        self.mongo_uri = os.getenv("MONGODB_CONNECTION_STRING")
+        self.isRemote = not "127.0.0.1" in self.mongo_uri and not "localhost" in self.mongo_uri
+
+    def testRetrieveAll(self):
+
+        self.assertTrue(self.mongo_uri is not None)
+
+        client = MongoClient(self.mongo_uri)
+        collectionSongsOriginal = client.get_database('yts_db').get_collection('songs_original')
+
+        self.assertTrue(collectionSongsOriginal is not None)
+
+        countDocs = collectionSongsOriginal.count_documents({})
+        logger.info(f"Count of documents {countDocs}")
+        self.assertTrue(countDocs > 50000 and countDocs < 60000, "Count of documents is not correct")
+
+    def testQueryMongoDB(self):
+
+        self.helperTestQueryMongoDB(isRemote=self.isRemote)
+
+
+    def helperTestQueryMongoDB(self, isRemote):
+
+        self.assertTrue(self.mongo_uri is not None)
+        client = self.getClient(isRemote)
+        collectionSongsOriginal = client.get_database('yts_db').get_collection('songs_original')
+        result: Cursor = collectionSongsOriginal.find({"artist": "Zwan"})
+        print("Result ", result.collection)
+        self.assertTrue(result is not None)
+        self.assertTrue(result.collection.count_documents({}) > 0)
+        ## Cursor to list
+        resultList = list(result)
+        songName = "Desire".lower()
+        print("Result list ", resultList)
+        filterSongs = [aSong for aSong in resultList if aSong["song"].lower() == songName]
+        print("Filter songs ", filterSongs)
+        self.assertTrue(filterSongs is not None)
+        self.assertTrue(len(filterSongs) > 0)
+
+    def getClient(self, isRemote):
+        if isRemote:
+            logger.info(
+                "Using remote connection: remember to add your IP address in the MongoDB Atlas site: cloud.mongodb.com/")
+            client = MongoClient(self.mongo_uri, tlsCAFile=certifi.where())
+        else:
+            client = MongoClient(self.mongo_uri)
+        return client
+
+
+    def testPingMongoDB(self):
+            client = self.getClient(isRemote=self.isRemote)
+            try:
+                r = client.admin.command('ping')
+                self.assertTrue("ok" in r)
+            except Exception as e:
+                print(e)
+                self.fail("Could not connect to MongoDB Atlas")
+
+if __name__ == '__main__':
+    unittest.main()
