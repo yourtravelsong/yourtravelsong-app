3dccaf05f318cce876ba1eec44323680b379a07c
Matias Martinez <matomartinez@gmail.com>
2024-09-20 11:09:16 +0200

    add Amadeus test

--- UnitTests.py
+++ UnitTests.py
@@ -1,10 +1,17 @@
+import os
 import time
 import unittest
 import json
 import requests
 import ollama
+from amadeus import ResponseError, Client
+from dotenv import load_dotenv
+
 
 class UnitTestCase(unittest.TestCase):
+
+    def setUp(self):
+        load_dotenv("index.env")
 
     def test_Suggestion_SingleCall(self):
         response = requests.post(headers={"Content-Type": "application/json"},
@@ -25,6 +32,39 @@ class UnitTestCase(unittest.TestCase):
 
         dataString = json.dumps(res, indent=4)
         print(dataString)
+
+
+    def testAmadeusAPI(self):
+        apikey = os.getenv("AMADEUS_API_KEY")
+        apisecret = os.getenv("AMADEUS_SECRET_KEY")
+
+        self.assertIsNotNone(apikey)
+        self.assertIsNotNone(apisecret)
+
+        amadeus = Client(
+            client_id=apikey,
+            client_secret=apisecret
+        )
+
+        try:
+
+            response = amadeus.shopping.flight_offers_search.get(
+                originLocationCode='MAD',
+                destinationLocationCode='ATH',
+                departureDate='2024-11-01',
+                adults=1)
+
+            self.assertTrue(len(response.data) > 0)
+
+            # https://developers.amadeus.com/self-service/category/flights/api-doc/airline-code-lookup/api-reference
+            response = amadeus.get('/v1/reference-data/airlines', airlineCodes='LH')
+
+            self.assertTrue(len(response.data) > 0)
+            self.assertTrue((response.data[0]["businessName"] is not None))
+
+        except ResponseError as error:
+            print(error.response.body)
+            self.fail(error.response.body)
 
 
     def test_ollama_chat(self):
