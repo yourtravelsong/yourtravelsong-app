f543ef7cefe3d7d30067c881e9d9ef31c6ca4b78
Matias Martinez <matomartinez@gmail.com>
2024-09-23 15:03:58 +0200

    add new test case: get invocation to the entry point

--- test/TestAPI.py
+++ test/TestAPI.py
@@ -15,7 +15,28 @@ class APIUnitTestCase(unittest.TestCase):
         if os.getenv("MONGODB_CONNECTION_STRING") is None and os.path.exists(arguments.env_file):
             load_dotenv(arguments.env_file)
 
-    def test_Suggestion_SingleCall(self):
+    def test_Suggestion_SingleCall_GET(self):
+
+        response = requests.get( url="http://127.0.0.1:5000/getsuggestion", headers={"Content-Type": "application/json"},
+                                 params={'artist': 'Radiohead', 'title': 'Creep'})
+
+        res = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone("success", res['status'])
+        self.assertIsNotNone(res['result'])
+        self.assertIsNotNone(res['result']['recommendations'])
+        self.assertIsNotNone(res['result']['sentiments'])
+        self.assertTrue(len(res['result']['recommendations']) > 0)
+        self.assertTrue(len(res['result']['sentiments']) > 0)
+
+        self.assertTrue("offers" in res['result']['recommendations'][0])
+        self.assertTrue(len(res['result']['recommendations'][0]["offers"]) > 0)
+
+        dataString = json.dumps(res, indent=4)
+        print(dataString)
+
+    def test_Suggestion_SingleCall_POST(self):
+        #### TODO: we will use the POST method to send the song lyrics as input, not implemented yet
         response = requests.post(headers={"Content-Type": "application/json"},
                                  json={'artist': 'Radiohead', 'title': 'Creep'},
                                  url="http://127.0.0.1:5000/getsuggestion")
