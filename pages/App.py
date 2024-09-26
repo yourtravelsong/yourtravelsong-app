829c98baeef6307e8ddb526915c99568b42982ee
Matias Martinez <matomartinez@gmail.com>
2024-09-26 19:30:44 +0200

    Improving front with new layout

--- pages/App.py
+++ pages/App.py
@@ -1,7 +1,8 @@
+import json
+
 import streamlit as st
 import requests
 import os
-import pandas as pd
 
 UNSPLASH_API_KEY = os.getenv("UNSPLASH_API_KEY")
 
@@ -36,22 +37,60 @@ def print_city_img(city: str) -> None:
     except Exception as e:
        print("Error {}".format(e))
 
-def write_results(cities: list[str], expl: str) -> None:
-    if (len(cities) > 0):
-        st.divider()
-        st.write("Top result:")
-        st.write(f"## {cities[0]}")
-        st.write("")
-        if (expl):
-            st.caption(expl)
-        #print_city_img(cities[0])
-        if (len(cities) != 0):
-            st.write("#### Other recommendations:")
-            for city in cities[1:]:
-                st.write(f"· {city}")
+def write_results(results) -> None:
+    st.write(f"# You are listening {results['result']['song']} by {results['result']['artist']}")
+    st.write(f"## Feeling from the song: {' - '.join(results['result']['sentiments'])}")
+
+    if (len(results['result']['recommendations']) > 0):
 
 
-def start_exec(song, author, results):
+        for aRecomentation  in results['result']['recommendations']:
+
+            aCity = aRecomentation["city"]
+
+            st.write(f"## Suggestion: Visit {aCity}")
+            st.write("")
+            st.write(f"### Why this suggestion? {aRecomentation['reason']}")
+            st.write("")
+
+            st.write(f"## Travel options to {aCity}:")
+            st.divider()
+            for i, offer in enumerate(aRecomentation["offers"]):
+
+                    #st.header(f"Travel plan idea #{i}")
+
+                    col1, col2, col3 = st.columns([2, 6, 1])  # The numbers represent the width ratio of the columns
+
+                    # Place the image in the first column
+
+
+                    # Place the link in the second column
+
+
+                    logoPath = f"/Users/matias/develop/code/yourtravelsong-app/airline-logos/logos/{offer['airline_icao_code']}.png"
+
+                    with col1:
+                       #st.image("your_image.png", width=100)  # Replace with the path to your image
+                        if os.path.exists(logoPath):
+                            #st.image(logoPath, None, 500)
+
+                            st.image(logoPath)
+
+                        else:
+                            st.write(f"Airline logo not found for {logoPath}")
+                    with col2:
+                        st.write(
+                            f"### {offer['departure']} - {offer['price']} {offer['currency']}")
+
+                    with col3:
+                        #st.markdown("[BUY](https://www.edreams.com/travel/#results/type=R;from=BCN;to=PAR;dep=2024-09-27;ret=2024-10-22;buyPath=FLIGHTS_HOME_SEARCH_FORM;internalSearch=true)")
+                        st.markdown(
+                                 '<a href="https://www.edreams.com/travel/#results/type=R;from=BCN;to=PAR;dep=2024-09-27;ret=2024-10-22;buyPath=FLIGHTS_HOME_SEARCH_FORM;internalSearch=true" style="font-size:20px;">BUY</a>',
+                                 unsafe_allow_html=True
+                             )
+
+
+def parseResults(song, author, results):
     # get cities from each result
     recommended_cities = []
     for res in results["result"]["recommendations"]:
@@ -80,28 +119,35 @@ def entry_point_request(song, author):
 def get_input() -> str:
     #get input from user (song - author)
     song = st.text_input("What is the name of a song that you're listening to right now?")
-    if not song:
-        st.stop()
+    #if not song:
+    #    st.stop()
     author = st.text_input("What is the author?")
-    if not author:
-        st.stop()
-    st.success('Name and author correctly submitted. Thanks!')
+
+    #if not author and not song:
+    #    st.stop()
+    #st.success('Name and author correctly submitted. Thanks!')
     return song, author
 
-def main() -> None:
-    st.title("YourTravelSong")
-    st.divider()
 
-    # get input from streamlit and start request to entry point
-    song, author = get_input()
+def main():
+    with st.form("mainForm"):
+        # Add two text fields
+        song = st.text_input("What is the name of a song that you're listening to right now?")
+        author = st.text_input("What is the author?")
 
-    # get the results
-    results = entry_point_request(song, author)
+        # if not song:
+        #    st.stop()
 
-    # display output
-    cities, explanation = start_exec(song, author, results)
-    write_results(cities, explanation)
-    travel_plan(results)
+        # Add a submit button
+        clicked = st.form_submit_button("Submit")
+
+        # Once the form is submitted
+        if clicked:
+            results = entry_point_request(song, author)
+
+            #print(json.dumps(results, indent=4))
+            write_results(results)
+
 
 if __name__ == "__main__":
     main()
